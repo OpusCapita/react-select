2974a513969a9fbb7dcba0c80020e8b724eabcd5
Kirill Volkovich <volkovich@scand.com>
2017-08-24 17:03:31 +0300

    Update gh-pages

--- README.md
+++
@@ -1,7 +0,0 @@
-## Synopsis
-
-* Add OpusCapita specific styles
-
-## License
-
-A short snippet describing the license (MIT, Apache, etc.)
--- src/client/index.js
+++
@@ -1,8 +0,0 @@
-// Here should be CommonJS component exports.
-
-module.exports = {
-  default: require('./components/Select').default,
-  Async: require('./components/Async').default,
-  AsyncCreatable: require('./components/AsyncCreatable'),
-  Creatable: require('./components/Creatable').default
-};
