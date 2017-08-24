648feebb065f4f868636146955ae702fb8ece783
Kirill Volkovich <volkovich@scand.com>
2017-08-24 17:57:47 +0300

    Fix exports

--- src/client/index.js
+++ src/client/index.js
@@ -1,11 +1,15 @@
-import Select from './components/Select';
-import Async from './components/Async';
-import AsyncCreatable from './components/AsyncCreatable';
-import Creatable from './components/Creatable';
+import Select from './componentns/Select';
+import Async from './componentns/Async';
+import AsyncCreatable from './componentns/AsyncCreatable';
+import Creatable from './componentns/Creatable';
+
+Select.Async = Async;
+Select.AsyncCreatable = AsyncCreatable;
+Select.Creatable = Creatable;
 
 export default Select;
 export {
-  Async,
-  AsyncCreatable,
-  Creatable
-}
+	Async,
+	AsyncCreatable,
+	Creatable
+};
