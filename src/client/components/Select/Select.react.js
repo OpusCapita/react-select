1969f44b02f5fe9a0c8f4f0dd03e89c1bbff953c
Kirill Volkovich <volkovich@scand.com>
2017-08-24 15:55:55 +0300

    Update

--- src/client/components/Select/Select.react.js
+++ src/client/components/Select/Select.react.js
@@ -1,29 +1,16 @@
 import React, { Component, PropTypes } from 'react';
 import ReactSelect from 'react-select';
-import './SelectDefault.less';
 import './SelectCustom.less';
 
 export default
 class Select extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null
-    };
-  }
-
-  handleChange(value) {
-    console.log(value);
-    this.setState({ value });
-  }
-
   render() {
-    let { className, ...restProps } = this.props;
-    let element = React.createElement(ReactSelect);
-    console.log(element);
+    let { className, autosize, ...restProps } = this.props;
+
     return (
       <ReactSelect
-        className={`opuscapita_react-select--1-0-0-rc-3 ${className}`}
+        className={`opuscapita_react-select--1-0-0-rc-5 ${className}`}
+        autosize={autosize}
         { ...restProps }
       />
     );
@@ -31,8 +18,10 @@ class Select extends Component {
 }
 
 Select.propTypes = {
-  className: PropTypes.string
+  className: PropTypes.string,
+  autosize: PropTypes.bool
 };
 Select.defaultProps = {
-  className: ''
+  className: '',
+  autosize: false
 };
