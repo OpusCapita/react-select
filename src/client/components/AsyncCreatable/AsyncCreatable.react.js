507cff19598cd0c1072d92e4c9b58317a28ac04e
Kirill Volkovich <kirill.volkovich@jcatalog.com>
2017-12-14 19:46:45 +0300

    Drop React v. 0.14.x support

--- src/client/components/AsyncCreatable/AsyncCreatable.react.js
+++ src/client/components/AsyncCreatable/AsyncCreatable.react.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import { AsyncCreatable as ReactAsyncCreatable } from 'react-select';
 import '../Select/SelectDefault.less';
 import '../Select/SelectCustom.less';
--- src/client/components/Select/Select.react.js
+++ src/client/components/Select/Select.react.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import ReactSelect from 'react-select';
 import './SelectDefault.less';
 import './SelectCustom.less';
