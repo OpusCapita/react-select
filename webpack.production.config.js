fc6eb25ad3eabfb8c3ba42b7eacf39b21d1629d0
Alexey Sergeev <sab@scand.com>
2018-05-15 16:54:12 +0300

    Fixing build process [skip ci]

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,7 +1,6 @@
 'use strict';
 const path = require('path');
 const webpack = require('webpack');
-let BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 let config = require('./webpack.development.config.js');
 config.entry = path.resolve(path.join(__dirname, 'src', 'client', 'index.js'));
