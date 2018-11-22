04ac00f552489f98431ce3d8e9d84cde53010105
Joel Luukka <32921638+jluukka@users.noreply.github.com>
2018-11-22 13:14:29 +0200

    Fix property 'isClearable' passing to react-select components (#9) (#10)

--- src/client/components/AsyncCreatable/AsyncCreatable.react.js
+++ src/client/components/AsyncCreatable/AsyncCreatable.react.js
@@ -7,6 +7,7 @@ import MenuPortal from '../MenuPortal__fix.react';
 export default ({ innerRef = () => {}, className = '', isClearable = true, ...restProps }) => (
   <ReactAsyncCreatable
     ref={innerRef}
+    isClearable={isClearable}
     className={`opuscapita_react-select--2-0-0 ${className}`}
     classNamePrefix="opuscapita_react-select"
     components={{ MenuPortal }}
